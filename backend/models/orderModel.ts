a915844b606a11a46a9eeb2a0f7fc1a536da8ae5
Brandon Chung <chung.brandon123@gmail.com>
2022-06-16 23:50:39 -0500

    Functionality for creating orders

---
+++ backend/models/orderModel.ts
@@ -0,0 +1,73 @@
+import { Types, Schema, model } from "mongoose";
+
+interface Order {
+	user: Types.ObjectId;
+	customerName: string;
+	purchasedItems: [
+		{
+			id: Schema.Types.ObjectId;
+			name: string;
+			quantity: number;
+			image: string;
+			price: number;
+		}
+	];
+	shippingAddress: {
+		address: string;
+		city: string;
+		country: string;
+		postalCode: string;
+	};
+	paymentDetails: {
+		paymentType: string;
+		cardNumber: string;
+		totalCost: number;
+		datePurchased: string;
+	};
+}
+
+const orderSchema = new Schema<Order>(
+	{
+		user: {
+			type: Schema.Types.ObjectId,
+			ref: "User",
+		},
+		customerName: {
+			type: String,
+			required: [true, "Name is required"],
+			ref: "User",
+		},
+		shippingAddress: {
+			address: { type: String, required: true },
+			city: { type: String, required: true },
+			country: { type: String, required: true },
+			postalCode: { type: String, required: true },
+		},
+		purchasedItems: [
+			{
+				id: {
+					type: Schema.Types.ObjectId,
+					required: true,
+					ref: "Product",
+				},
+				name: { type: String, required: true, ref: "Product" },
+				quantity: { type: Number, required: true },
+				image: { type: String, required: true, ref: "Product" },
+				price: { type: Number, required: true, ref: "Product" },
+			},
+		],
+		paymentDetails: {
+			paymentType: { type: String, required: true },
+			cardNumber: { type: String, required: true },
+			totalCost: { type: Number, required: true },
+			datePurchased: { type: String, required: true },
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const OrderModel = model<Order>("Order", orderSchema);
+
+export default OrderModel;
---
+++ backend/routes/order/orderRoutes.ts
@@ -0,0 +1,8 @@
+import express, { IRouter } from "express";
+import verifyJWT from "../../middleware/authJWT";
+import validateRequest from "../../middleware/validateReq";
+
+const router: IRouter = express.Router();
+
+// router.get('')
+export default router;
