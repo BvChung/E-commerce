f4601cbbbc644151eaff91ed76cfef4d5da017be
Brandon Chung <chung.brandon123@gmail.com>
2022-06-23 22:22:17 -0500

    Update dependencies

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -31,5 +31,18 @@ module.exports = {
 			},
 		},
 	},
-	plugins: [],
+	daisyui: {
+		themes: ["lofi", "dark"],
+	},
+	daisyui: {
+		styled: true,
+		themes: true,
+		base: true,
+		utils: true,
+		logs: true,
+		rtl: false,
+		prefix: "",
+		darkTheme: "dark",
+	},
+	plugins: [require("daisyui")],
 };
